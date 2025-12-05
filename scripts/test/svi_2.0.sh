74b0b2cbd3cdc5aa59eb623b0239aaeaef2fa7e6
Rajiv <millord237@gmail.com>
2025-12-05 10:46:58 +0800

    [Update] SVI-2.0 Inference Script

--- scripts/test/svi_2.0.sh
+++ scripts/test/svi_2.0.sh
@@ -6,7 +6,8 @@ python test_svi.py \
 --ref_pad_num -1 \
 --cfg_scale_text 5.0 \
 --num_motion_frames 1 \
---ref_image_path data/toy_test/shot/frame.jpg \
---prompt_path data/toy_test/shot/prompt.txt \
---use_first_prompt_only \
+--num_clips 100 \
+--ref_image_path data/toy_test/svi_2.0/frame.jpg \
+--prompt_path data/toy_test/svi_2.0/prompt.txt \
+--prompt_repeat_times 999 \
 --extra_module_root weights/Stable-Video-Infinity/version-2.0/SVI_Wan2.1-I2V-14B_lora_v2.0.safetensors
